985ae74d8760e0cee68f67c82a292517a82566dc
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-07-29 14:49:28 -0500

    entendì el manejo de interfacez

--- JavaPropio/exagonal/src/main/java/application/CreateUserUseCase.java
+++ JavaPropio/exagonal/src/main/java/application/CreateUserUseCase.java
@@ -2,19 +2,25 @@ package application;
 
 import domain.entity.User;
 import domain.service.UserService;
+import infraestructure.out.UserRepository;
 
 public class CreateUserUseCase {
+/* 
+    private final UserService userService; */
 
-    private final UserService userService;
 
-
-    public CreateUserUseCase (UserService userService ) {
+/*     public CreateUserUseCase (UserService userService ) {
         this.userService = userService;
         
-    } 
-
+    }  */
+/* 
     public void executeCreateUser (User user) {
         userService.createUser(user);
-    }
+    } */
 
+
+    public void executeCreateUser (User user) {
+        UserRepository userRepository = new UserRepository(); 
+        userRepository.createUser(user);
+    }
 }
