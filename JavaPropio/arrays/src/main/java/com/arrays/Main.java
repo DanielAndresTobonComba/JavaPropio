558009425854a69ccb35057acf71e0063c56ba2a
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-07-29 16:55:32 -0500

    interador

---
+++ JavaPropio/arrays/src/main/java/com/arrays/Main.java
@@ -0,0 +1,8 @@
+package com.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        System.out.println(myNumbers[0][1]);
+    }
+}
